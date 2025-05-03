7a99408105e9078e214dc15c1696545e8ea268b3
CamiloMedinaH <camilo.medinah@udea.edu.co>
2025-05-03 14:44:39 -0500

    Se implemento una barra de navegacion

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practica_2_gitflow/pages/area_circulo.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,34 +9,83 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  int _selectedIndex = 0;
+  final List<Widget> _pages = [
+    AreaCirculo(),
+    PerimetroCirculo(),
+    AreaCuadrado(),
+    PerimetroCuadrado(),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text("Que quieres calcular?"),
-              TextButton(
-                  onPressed: () {},
-                  child: const Text("Area Circulo")
-              ),
-              TextButton(
-                  onPressed: () {},
-                  child: const Text("Perimetro Circulo")
-              ),
-              TextButton(
-                  onPressed: () {},
-                  child: const Text("Area Cuadrado")
-              ),
-              TextButton(
-                  onPressed: () {},
-                  child: const Text("Perimetro Cuadrado")
-              )
-            ],
-          ),
-        ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: _onTap,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle), label: 'Area Circulo'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle_outline), label: 'Perimetro Circulo'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_box_rounded), label: 'Area Cuadrado'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_box_outlined), label: 'Perimetro Circulo'),
+        ],
       ),
     );
   }
 }
+/* Funcion para calcular el area
+  class AreaCirculo extends StatelessWidget {
+    @override
+    Widget build(BuildContext constext) {
+      return Scaffold(
+        appBar: AppBar(title: Text('Perimetro circulo')),
+        body: Center(child: Text('Perimetro')),
+      );
+    }
+  }
+*/
+
+  class PerimetroCirculo extends StatelessWidget {
+    @override
+    Widget build(BuildContext constext) {
+      return Scaffold(
+        appBar: AppBar(title: Text('Perimetro circulo')),
+        body: Center(child: Text('Perimetro')),
+      );
+    }
+  }
+
+  class AreaCuadrado extends StatelessWidget {
+    @override
+    Widget build(BuildContext constext) {
+      return Scaffold(
+        appBar: AppBar(title: Text('Area cuadrado')),
+        body: Center(child: Text('Area')),
+      );
+    }
+  }
+
+
+  class PerimetroCuadrado extends StatelessWidget {
+    @override
+    Widget build(BuildContext constext) {
+      return Scaffold(
+        appBar: AppBar(title: Text('Perimetro cuadrado')),
+        body: Center(child: Text('Perimetro')),
+      );
+    }
+  }
+
